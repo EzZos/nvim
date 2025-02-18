c843152bbe77b7e356335e8baf4186060905ca8a
João Zumaêta <ezzoscode@gmail.com>
2025-02-18 11:07:56 -0300

    feat(treesitter): add tsx, html, css, json

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 	build = ":TSUpdate",
 	opts = {
 		-- A list of parser names, or "all" (the listed parsers MUST always be installed)
-		ensure_installed = { "javascript", "typescript", "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+		ensure_installed = { "javascript", "typescript","tsx", "json", "css", "html", "c", "cpp", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
 		-- Install parsers synchronously (only applied to `ensure_installed`)
 		sync_install = false,
